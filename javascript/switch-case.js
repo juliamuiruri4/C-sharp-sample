e5b257a4f080302352f1e77563ba3e4bc27879fa
Julia Muiruri <julia.muiruri4@gmail.com>
2022-10-17 23:35:51 +0300

    Added javascript simple switch case

---
+++ javascript/switch-case.js
@@ -0,0 +1,18 @@
+let input = 7;
+
+switch (input) {
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    default:
+        console.log("Invalid input");
+}
